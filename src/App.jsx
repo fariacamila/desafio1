2c4c9f362152fbfa6f022a9ad7550359b68a36f4
Camila Faria <faria.camila@live.com>
2022-09-02 18:13:15 -0300

    component proximo arrumado

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Header from './Header/Header.jsx'
 import Trailer from './Trailer/Trailer.jsx';
 import Proximo from './Proximo/Proximo.jsx';
 
+
 function App() {
   return (
     <section>
       <Header />
       <Trailer />
-      <Proximo />
+      < Proximo />
     </section>
   )
 }
